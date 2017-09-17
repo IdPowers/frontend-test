61bcfe442c44cab65defd9efbaee526a788b7e33
Andrei Riaskov <ryaskov.work@gmail.com>
2017-09-17 21:44:45 +0300

    feat(cart): done cart's view layout

--- client/src/blocks/header/header.js
+++ client/src/blocks/header/header.js
@@ -1,3 +1,17 @@
+/* eslint-disable */
 require('assets/icons/search.svg');
 require('assets/icons/cart.svg');
 require('assets/icons/user.svg');
+
+window.onCartClicked = function() {
+  const currentValue = document.getElementsByClassName('cart')[0].style.display;
+  if (currentValue === 'none') {
+    document.getElementsByClassName('cart')[0].style.display = 'block';
+    document.getElementsByClassName('header__left')[0].style.width = 'auto';
+    document.getElementsByClassName('header__right')[0].style.width = 'auto';
+  }else{
+    document.getElementsByClassName('cart')[0].style.display = 'none';
+    document.getElementsByClassName('header__left')[0].style.width = '880px';
+    document.getElementsByClassName('header__right')[0].style.width = '360px';
+  }
+}
